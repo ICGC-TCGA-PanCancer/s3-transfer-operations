bdc39175690bffddaeda41d1d846c14f1ab0dbda
Niall Byrne <reading.crows@gmail.com>
2015-07-17 13:02:22 -0400

    .

--- scripts/json2ini.py
+++ scripts/json2ini.py
@@ -46,7 +46,15 @@ def main():
     with codecs.open(template, 'r', 'utf-8') as f:
         template_data = f.read()
 
-    print pystache.render(template_data, handlebars)
+    # Final INI File
+    final_data = pystache.render(template_data, handlebars)
+
+    print final_data # For debugging
+
+    with codecs.open(os.path.join(output_folder, os.path.basename(json_input)) + ".ini", "w", 'utf-8') as f:
+        f.write(final_data)
+
+    sys.exit(0)
 
 if __name__ == '__main__':
     if len(sys.argv) != 4:
