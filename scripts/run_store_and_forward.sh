0d0379ac767615c1271c0bb470003bcc55b70946
Ubuntu <ubuntu@ip-172-31-33-211.ec2.internal>
2015-07-20 22:03:08 +0000

    Script to run workflow in docker

---
+++ scripts/run_store_and_forward.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -h master -it -v /var/run/docker.sock:/var/run/docker.sock -v /home/ubuntu/.gnos:/home/ubuntu/.gnos -v /datastore:/datastore -v /workflows:/workflows -v /workflows/workflow.ini:/workflow.ini seqware/seqware_whitestar_pancancer:1.1.1 bash -c "seqware bundle launch --ini /workflow.ini --dir /workflows/Workflow_Bundle_StoreAndForward_1.0.2_SeqWare_1.1.0/ --engine whitestar --no-metadata"
